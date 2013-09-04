d6e6bc41a53cee1d1cce70c42d259a01a3a1f7d0
Sanjay Kapoor <sanjay@obaz.com>
2013-09-04 13:26:37 -0500

    check for proc first, then default to regular ad

--- lib/ads/rails/action_view/base.rb
+++ lib/ads/rails/action_view/base.rb
@@ -5,7 +5,10 @@ module Ads
 
         def google_adsense_include_tag(*args)
           options = args.extract_options!
-          if ['production','test'].include? ::Rails.env
+          # first check for a special proc
+          if ::Rails.application.config.ads.renderer.is_a? Proc
+            instance_exec(options, &::Rails.application.config.ads.renderer)
+          else
             script = <<-SCRIPT
               google_ad_client = '#{options[:client]}';
               google_ad_slot = '#{options[:slot]}'; 
@@ -14,12 +17,6 @@ module Ads
             SCRIPT
             content_tag(:script, script.html_safe, type: 'text/javascript') +
             content_tag(:script, nil, type: 'text/javascript', src: 'http://pagead2.googlesyndication.com/pagead/show_ads.js')
-          elsif ::Rails.env.development?
-            if ::Rails.application.config.ads.renderer.is_a? Proc
-              instance_exec(options, &::Rails.application.config.ads.renderer)
-            else
-              content_tag(:div, nil, style: "width:#{options[:width]}px;height:#{options[:height]}px;background:#c8c8c8;")
-            end
           end
         end
 
